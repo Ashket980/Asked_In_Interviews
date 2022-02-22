907986cde96d21754fbac3a407eb9c1600110f2d
Ashish Kumar <85364791+Ashket980@users.noreply.github.com>
2022-02-22 13:57:00 +0530

    Create Python.py

---
+++ Find a pair with the given sum in an array/Python.py
@@ -0,0 +1,24 @@
+# Naive method to find a pair in a list with the given sum
+def findPair(nums, target):
+ 
+    # consider each element except the last
+    for i in range(len(nums) - 1):
+ 
+        # start from the i'th element until the last element
+        for j in range(i + 1, len(nums)):
+ 
+            # if the desired sum is found, print it
+            if nums[i] + nums[j] == target:
+                print('Pair found', (nums[i], nums[j]))
+                return
+ 
+    # No pair with the given sum exists in the list
+    print('Pair not found')
+ 
+ 
+if __name__ == '__main__':
+ 
+    nums = [8, 7, 2, 5, 3, 1]
+    target = 10
+ 
+    findPair(nums, target)
