b4bb7afbe351646319798b6ae9c6c457bea499aa
Ashish Kumar <85364791+Ashket980@users.noreply.github.com>
2022-02-22 14:16:33 +0530

    Create Java.java

---
+++ Find the next greater element for every array element/Java.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Stack;
+ 
+class Main
+{
+    // Find the next greater element for every array element
+    public static int[] findNextGreaterElements(int[] input)
+    {
+        // base case
+        if (input == null) {
+            return input;
+        }
+ 
+        int[] result = new int[input.length];
+        Arrays.fill(result, -1);
+ 
+        // create an empty stack
+        Stack<Integer> s = new Stack<>();
+ 
+        // do for each element
+        for (int i = 0; i < input.length; i++)
+        {
+            // loop till we have a greater element on top or stack becomes empty.
+ 
+            // Keep popping elements from the stack smaller than the current
+            // element, and set their next greater element to the current element
+ 
+            while (!s.isEmpty() && input[s.peek()] < input[i]) {
+                result[s.pop()] = input[i];
+            }
+ 
+            // push current "index" into the stack
+            s.push(i);
+        }
+ 
+        return result;
+    }
+ 
+    public static void main(String[] args)
+    {
+        int[] input = { 2, 7, 3, 5, 4, 6, 8 };
+ 
+        int[] result = findNextGreaterElements(input);
+        System.out.println(Arrays.toString(result));
+    }
+}
