2cc9061bef90aa1af22b3428c8c4d65fee6f877e
Ashish Kumar <85364791+Ashket980@users.noreply.github.com>
2022-03-15 22:27:20 +0530

    Create Python.py

---
+++ Print binary tree structure with its contents/Python.py
@@ -0,0 +1,65 @@
+# A Binary Tree Node
+class Node:
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+ 
+class Trunk:
+    def __init__(self, prev=None, str=None):
+        self.prev = prev
+        self.str = str
+ 
+def showTrunks(p):
+    if p is None:
+        return
+    showTrunks(p.prev)
+    print(p.str, end='')
+ 
+ 
+def printTree(root, prev, isLeft):
+    if root is None:
+        return
+ 
+    prev_str = '    '
+    trunk = Trunk(prev, prev_str)
+    printTree(root.right, trunk, True)
+ 
+    if prev is None:
+        trunk.str = '———'
+    elif isLeft:
+        trunk.str = '.———'
+        prev_str = '   |'
+    else:
+        trunk.str = '`———'
+        prev.str = prev_str
+ 
+    showTrunks(trunk)
+    print(' ' + str(root.data))
+    if prev:
+        prev.str = prev_str
+    trunk.str = '   |'
+    printTree(root.left, trunk, False)
+ 
+ 
+if __name__ == '__main__':
+ 
+    # Construct above tree
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+    root.left.left.left = Node(8)
+    root.left.left.right = Node(9)
+    root.left.right.left = Node(10)
+    root.left.right.right = Node(11)
+    root.right.left.left = Node(12)
+    root.right.left.right = Node(13)
+    root.right.right.left = Node(14)
+    root.right.right.right = Node(15)
+ 
+    # print constructed binary tree
+    printTree(root, None, False)
